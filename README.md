9cf5cee1778bd498e077049acbc11a704e60a9f8
wheatwhole <117047051+wheatwhole@users.noreply.github.com>
2025-09-21 03:05:24 +0000

    fix: improved structure to prepare for a PyPI package #1

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # galapagos
 python package for codehs that makes Tracy the turtle better 
+i just made this for highschool idk
---
+++ __main__.py
@@ -0,0 +1,9 @@
+# __main__.py
+
+from galapagos import *
+
+def main():
+    speed(base_speed)
+    
+if __name__ == "__main__":
+    main()
